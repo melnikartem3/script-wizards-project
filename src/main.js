694cf1ea90ab19434faae810b4bc6e32ca1ea091
Artem Melnik <melnikartem@vesele-gimnaz.com>
2024-03-17 09:40:10 +0200

    add libs

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import Accordion from 'accordion-js';
+import 'accordion-js/dist/accordion.min.css';
+import Swiper from 'swiper';
+import { Navigation, Pagination, Scrollbar } from 'swiper/modules';
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
