b1b7bee51c4d4016de3a8547f502ffab1f2f07d2
Daria2608 <dariya.pitra@gmail.com>
2024-03-17 18:42:42 +0200

    faq-section-2
    Перероблений html + css з урахуванням помилок, +js

---
+++ src/js/faq.js
@@ -0,0 +1,26 @@
+import Accordion from 'accordion-js';
+import 'accordion-js/dist/accordion.min.css';
+
+// витягуємо елементи, з якими будемо працювати 
+const faqColumnLine = document.querySelector('.faq-column-line');
+const faqListItems = document.querySelectorAll('.faq-list-item');
+const faqList = document.querySelector('.faq-list')
+
+const faqBtns = document.querySelectorAll('.faq-list-item-btn');
+
+faqBtns.forEach(function(faqBtn) {
+    faqBtn.addEventListener('click', function () {
+        
+        const faqListItem = faqBtn.parentElement;
+        const faqText = faqListItem.querySelector('.faq-list-item-text');
+        const faqSvg = faqBtn.querySelector('.faq-svg')
+ 
+        if (faqText.style.display === 'none' || !faqText.style.display) {
+            faqText.style.display = 'block';
+            faqSvg.classList.add('close-svg')
+        } else {
+            faqText.style.display = 'none';
+            faqSvg.classList.remove('close-svg')
+        }
+    });
+});
