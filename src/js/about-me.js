3f3dda4ac8999e8e9aa72d8f7e9880c20e6a8200
Step004 <ctepan.filep9@gmail.com>
2024-03-19 00:35:34 +0200

    add swiper

--- src/js/about-me.js
+++ src/js/about-me.js
@@ -3,9 +3,9 @@ const accordionPanels = document.querySelector('.accordion-panel');
 const firstContainer = document.querySelector('.about-ac');
 const firstIcon = document.querySelector('.mobile-open-ac');
 
-      accordionPanels.style.maxHeight =  '100%';
-      firstContainer.classList.add('active');
-      firstIcon.style.transform = 'rotate(180deg)';
+accordionPanels.style.maxHeight = '100%';
+firstContainer.classList.add('active');
+firstIcon.style.transform = 'rotate(180deg)';
 
 accordionHeaders.forEach(header => {
   header.addEventListener('click', function () {
@@ -16,8 +16,8 @@ accordionHeaders.forEach(header => {
     accordionItem.classList.toggle('active');
 
     if (accordionItem.classList.contains('active')) {
-        panel.style.maxHeight = panel.scrollHeight + 'px';
-        icon.style.transform = 'rotate(180deg)';
+      panel.style.maxHeight = panel.scrollHeight + 'px';
+      icon.style.transform = 'rotate(180deg)';
     } else {
       panel.style.maxHeight = '0';
       icon.style.transform = 'rotate(0deg)';
