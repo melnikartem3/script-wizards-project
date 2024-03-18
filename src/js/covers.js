3d91df764a42786a126542c4dd38c2247c699354
Krii <kriilunaus16@gmail.com>
2024-03-18 12:49:17 +0100

    add on adn off animation

--- src/js/covers.js
+++ src/js/covers.js
@@ -1,36 +1,44 @@
-`use strict`
+'use strict';
 
 const scroller = document.querySelectorAll(".covers-scroller-inner");
 const coversSection = document.querySelector(".covers");
 
-const arrayFromScroller = Array.from(scroller)
+const arrayFromScroller = Array.from(scroller);
 
 function addDoubleHtml() {
     arrayFromScroller.forEach((scrollerItem) => {
 
-      const scrollerInner = Array.from(scrollerItem.children);
-        
+        const scrollerInner = Array.from(scrollerItem.children);
+
         scrollerInner.forEach((item) => {
-                const duplicateItem = item.cloneNode(true);
-                duplicateItem.setAttribute("aria-hidden", true);
-                scrollerItem.appendChild(duplicateItem);
-            })
-        })
-    }
-    
-addDoubleHtml()
+            const duplicateItem = item.cloneNode(true);
+            duplicateItem.setAttribute("aria-hidden", true);
+            scrollerItem.appendChild(duplicateItem);
+        });
+    });
+}
 
-// const options = {
-//     threshold: 0.25,
-//     rootMargin: "",
-// };
+addDoubleHtml();
 
-// const observer = new IntersectionObserver(function  (entries, observer) {
-//     entries.forEach(entry => {
-    
-//     })
-    
-// }, options);
+const options = {
+    threshold: 0.25,
+    rootMargin: "0px",
+};
 
+const observer = new IntersectionObserver(function(entries, observer) {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            //animeton on
+            arrayFromScroller.forEach(scrollerItem => {
+                scrollerItem.style.animationPlayState = "running";
+            });
+        } else {
+            //animation off
+            arrayFromScroller.forEach(scrollerItem => {
+                scrollerItem.style.animationPlayState = "paused";
+            });
+        }
+    });
+}, options);
 
-// observer.observe(coversSection)
+observer.observe(coversSection);
