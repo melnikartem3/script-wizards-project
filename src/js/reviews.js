a4ba03d32c2afed68fb76f72ebe0122a04e90dc2
Denys Puzikov <denys.puzikov@gmail.com>
2024-03-19 22:30:07 +0100

    updates reviews script

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -1,23 +1,43 @@
 'use strict';
 
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
 import { swiperReviews } from './swiper.js';
 
 const URL = 'https://portfolio-js.b.goit.study/api/reviews';
 const list = document.querySelector('.reviews-list');
+const btnWrapper = document.querySelector('.reviews-btn-wrapper');
 
-fetch(URL)
-  .then(response => {
+async function fetchDataAndRender() {
+  try {
+    const response = await fetch(URL);
+
     if (!response.ok) {
       throw new Error(response.status);
     }
-    return response.json();
-  })
-  .then(data => {
+
+    const data = await response.json();
+
     list.innerHTML = renderCard(data);
 
-    swiperReviews;
-  })
-  .catch(error => console.log(error));
+    swiperReviews.update();
+  } catch (error) {
+    iziToast.error({
+      theme: 'dark',
+      message: 'Oops, sorry, something went wrong. Please try again later.',
+      messageColor: '#ffffff',
+      backgroundColor: '#ef4040',
+      position: 'topRight',
+      pauseOnHover: false,
+      progressBarColor: '#b51b1b',
+      timeout: 3000,
+    });
+
+    list.innerHTML = '<span class="reviews-plug">Not found 😔</span>';
+
+    btnWrapper.style.display = 'none';
+  }
+}
 
 function renderCard(card) {
   return card
@@ -38,3 +58,28 @@ function renderCard(card) {
     })
     .join('');
 }
+
+function isElementInViewport(element) {
+  const rect = element.getBoundingClientRect();
+
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  const section = document.querySelector('.reviews-page');
+
+  let sectionReached = false;
+
+  window.addEventListener('scroll', function () {
+    if (!sectionReached && isElementInViewport(section)) {
+      sectionReached = true;
+      fetchDataAndRender();
+    }
+  });
+});
