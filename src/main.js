6955e0636d09101c3e832aadc8291888133d9fa4
Artem Melnik <melnikartem@vesele-gimnaz.com>
2024-03-19 14:57:18 +0200

    some fixes in hero and header

--- src/main.js
+++ src/main.js
@@ -6,11 +6,10 @@ import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-import './js/header'
-import './js/hero'
+import './js/header';
 import './js/projects';
 import './js/reviews';
-import './js/faq'
+import './js/faq';
 import './js/about-me';
 import './js/mobile-menu';
 import './js/work-together';
