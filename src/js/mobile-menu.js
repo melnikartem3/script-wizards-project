fdc03f5b299509d81aa882a51e449e7acad304af
Denys Puzikov <denys.puzikov@gmail.com>
2024-03-19 15:21:26 +0100

    removed scroll while open mobile menu

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -16,11 +16,6 @@
 
     openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
     mobileMenu.classList.toggle('is-open');
-
-    const scrollLockMethod = !isMenuOpen
-      ? 'disableBodyScroll'
-      : 'enableBodyScroll';
-    bodyScrollLock[scrollLockMethod](document.body);
   };
 
   openMenuBtn.addEventListener('click', toggleMenu);
